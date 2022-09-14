b0f204bd08ed24ae4ca839c4df32f65819fd5bbf
bg <62928419+bgeertsen@users.noreply.github.com>
2022-09-13 21:58:13 -0700

    finished gallery

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,6 +7,11 @@ function Nav(props) {
     document.title = capitalizeFirstLetter(currentCategory.name);
   }, [currentCategory]);
 
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
+
   return (
     <header className="flex-row px-1">
       <h2>
@@ -26,13 +31,11 @@ function Nav(props) {
             </a>
           </li>
           <li className="mx-2">
-            <span>Contact</span>
+            <span onClick={() => handleClick("Contact")}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && "navActive"
-              }`}
+              className={`mx-1 ${currentCategory.name === category.name}`}
               key={category.name}
             >
               <span
